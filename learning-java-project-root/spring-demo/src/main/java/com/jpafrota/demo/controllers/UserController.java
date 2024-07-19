916d61cedeba0c5be69ec4512ff1dbc75b11efa6
Jorge Frota <jorgepabloafrota@gmail.com>
2024-07-18 20:32:16 -0400

    spring-boot: Added user delete endpoint

--- learning-java-project-root/spring-demo/src/main/java/com/jpafrota/demo/controllers/UserController.java
+++ learning-java-project-root/spring-demo/src/main/java/com/jpafrota/demo/controllers/UserController.java
@@ -43,4 +43,10 @@ public class UserController {
                 .toUri();
         return ResponseEntity.created(uri).body(obj);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
